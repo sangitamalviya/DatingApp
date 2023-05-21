359276c2c5e7a19552021740427b772366da275d
DESKTOP-PASTEQH\Sangita <sangita.malviya@3disystems.com>
2023-05-21 22:27:51 +0530

    test

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<DataContext>(opt=>
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
